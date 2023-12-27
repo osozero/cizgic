95d89a201bb719c0c4d20fdd7a00ce94a8d1798a
samet <sametozer13@gmail.com>
2023-12-27 00:16:11 -0800

    load texture and draw it as is

--- source.c
+++ source.c
@@ -16,11 +16,203 @@ typedef struct {
   float  y;
 } Point;
 
+typedef struct {
+  int width;
+  int height;
+  int num_of_bits_per_pixel;
+
+  // format of pixels is bottom to up while ppm file format is top to bottom.
+  char * pixels;
+} Texture;
+
 int draw_triangle(size_t * pixels, int w, int h,Point *p1, Point *p2, Point *p3, int color, char * output_filename);
 
 int compare_f(const void *p1, const void *p2) {
   return (**(Point**)p1).y - (**(Point**)p2).y;
 }
+
+Texture* load_texture(char* image_path) {
+  FILE *file = fopen(image_path, "rb");
+
+  if (file == NULL) {
+    printf("file could not read: %s\n", image_path);
+    exit(1);
+  }
+
+  fseek(file, 0, SEEK_END);
+  size_t file_len = ftell(file);
+  fseek(file, 0, SEEK_SET);
+
+  char *buffer = (char*) malloc(file_len * sizeof(char));
+
+  if(!buffer) {
+    printf("malloc error\n");
+    fclose(file);
+    exit(1);
+  }
+
+  size_t count = fread(buffer, sizeof(char), file_len, file);
+
+  while(count < file_len) {
+    if(count == 0) {
+      //read error or EOF
+      if (feof(file)) {
+	break;
+      } else {
+	printf("read file error\n");
+	fclose(file);
+	exit(1);
+      }
+    } else {
+      file_len -= count;
+      count = fread(buffer+count, file_len, sizeof(char), file);
+    }
+  }
+
+  if(buffer[0] != 'B' && buffer[1] != 'M') {
+    printf("File must be bmp: %s\n", image_path);
+    fclose(file);
+    exit(1);
+  }
+
+  
+  int file_size = *(int*)(buffer+2);
+  int width_of_pixels = *(int*)(buffer + 0x12);
+  int height_of_pixels = *(int*)(buffer + 0x16);
+
+  short  num_of_bits_per_pixel = *(short*)(buffer + 0x1C);
+
+  int image_pixel_offset = *(int*)(buffer+0xA);
+  int total_size_of_pixels_data = file_size - image_pixel_offset;
+  
+  char *pixels = (char*)malloc(total_size_of_pixels_data * sizeof(char));
+  if (!pixels) {
+    printf("malloc error\n");
+    fclose(file);
+    exit(1);
+  }
+
+  size_t index = 0;
+  int curr = image_pixel_offset;
+  
+  while(curr-image_pixel_offset <= total_size_of_pixels_data) {
+    int padding = 0;
+    for (int i = 0; i < width_of_pixels * (num_of_bits_per_pixel/8); i+=3 ) {
+      // Pixel format in BMP is little endian so red is kept as 00 00 FF
+      pixels[index+2] = *(buffer+curr);
+      pixels[index+1] = *(buffer+curr+1);
+      pixels[index] = *(buffer+curr+2);
+      curr+=3;
+      index += 3;
+      padding+=3; // assumig number of bits per pixel is 24.
+    }
+    padding = padding % 4;
+    while(padding > 0) {
+      curr ++;
+      padding--;
+    }
+  }
+
+
+  Texture *t = (Texture*)malloc(sizeof(Texture));
+
+  if (!t) {
+    printf("malloc error for texture\n");
+    fclose(file);
+    exit(1);
+  }
+
+  t->pixels = pixels;
+  t->height = height_of_pixels;
+  t->width = width_of_pixels;
+  t->num_of_bits_per_pixel = num_of_bits_per_pixel;
+  
+  fclose(file);
+  free(buffer);
+
+  return t;
+  
+}
+
+
+int save_texture_as_ppm(Texture *t, char* output_filename) {
+  
+  FILE* file = fopen(output_filename, "wb");
+  if (file == NULL){
+    printf("error opening file\n");
+    return -1;
+  }
+  
+  fprintf(file, "P6\n%d %d\n%d\n", t->width, t->height, 255);
+
+
+  int len = t->width * t->height*3;
+  char buf[len];
+
+  /* Format of t->pixels is slightly different than PPM format.
+     While pixels are processed top to bottom in PPM format, it is bottom to up in t->pixels.
+     That's the reason we need to find correspoinding pixel in t-pixels by doing simple math
+  */
+  for (int i  = 0; i < len; i+=3) {
+    int y_offset = t->height -  (i/3)/(t->height) - 1;
+    
+    int x_offset = (i/3) % t->width;
+
+    int offset = x_offset + y_offset*t->width;
+    offset*=3;
+   
+    buf[i] = t->pixels[offset];
+    buf[i+1] = t->pixels[offset +1];
+    buf[i+2] = t->pixels[offset +2];
+  }
+  
+  for(int i=0; i<t->width * t->height*3; i+=3) {
+    fwrite(buf + i, 3, 1, file);
+  }
+
+  fclose(file);
+
+  return 0;  
+
+}
+
+int draw_rectangle_with_texture(Texture *t, int w, int h, char *output_filename) {
+  FILE* file = fopen(output_filename, "wb");
+  if (file == NULL){
+    printf("error opening file\n");
+    return -1;
+  }
+  
+  fprintf(file, "P6\n%d %d\n%d\n", w, h, 255);
+
+
+  int byte_per_pixel = t->num_of_bits_per_pixel/8;
+
+  int len = t->width * t->height*3;
+  char buf[len];
+  for (int i  = 0; i < len; i+=3) {
+    int y_offset = t->height -  (i/3)/(t->height) - 1;
+    
+    int x_offset = (i/3) % t->width;
+
+    int offset = x_offset + y_offset*t->width;
+    offset*=3;
+   
+    buf[i] = t->pixels[offset];
+    buf[i+1] = t->pixels[offset +1];
+    buf[i+2] = t->pixels[offset +2];
+  }
+
+  for(int i=0; i<t->width * t->height*3; i+=3) {
+    fwrite(buf + i, 3, 1, file);
+  }
+  
+  fclose(file);
+
+
+  return 0;
+}
+
 
 int draw_rectangle(int width, int height, int color, char* output_filename)
 {
@@ -288,6 +480,19 @@ int draw_dots(size_t * pixels, int w, int h, int x1, int y1, int x2, int y2, int
 }
 
 int main() {
+  Texture * texture  = load_texture("/Users/oso/Desktop/texture_mapping/bmp_24.bmp");
+  if (!texture) {
+
+    printf("errrrrrrrrr\n");
+    free(texture);
+
+    exit(1);
+  }
+
+  draw_rectangle_with_texture(texture, 100, 100, "rectangle_with_texture_smaller.ppm");
+
+  return 0;
+
   // Note that os x is  little indian so 0x000000ff is kept in memory as 0xff000000
   // which I would like to interpret as RGB (ff, 00, 00) for ppm format
   draw_rectangle(10, 10, 0x000000ff, "rect.ppm");
@@ -342,7 +547,3 @@ int main() {
   draw_triangle_in_a_better_way(pixels,w, h, &p12, &p13, &p14, 0x000000ff, false,  "triangle_irregular_no_fill_test.ppm");
   return 0;
 }
-
-
-
-
